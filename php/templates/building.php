de5fb1fa91df67cba1b4e1c5fc1176c6739f884b
jovicon <jocontrera16@gmail.com>
2017-10-23 20:58:55 -0300

    uploading developing message

---
+++ php/templates/building.php
@@ -0,0 +1,24 @@
+<?php
+
+  $building = '';
+  $building = '
+                <section class="full-screen">
+                  <div class="hero-img"></div>
+
+                  <div class="fixed-layer">
+
+                    <p class="hero-title">Hi! <br> My name is Jose Contreras.</p>
+                    <p class="hero-text">I\'m happy about your visit but actually, I\'m developing my website.</p>
+                    <p class="hero-text">So, You can get in touch here: </p>
+                    <a class="hero-text" href="mailto:jocontrera16@gmail.com">Jocontrera16@gmail.com</a>
+                    <p class="hero-text"> <a href="https://api.whatsapp.com/api/send?phone=56972216367">+56972216367</a> </p>
+                    <p class="hero-text"> <a href="https://github.com/jovicon">Github: Jovicon</a> </p>
+
+                  </div>
+
+                </section>
+              ';
+
+  echo $building;
+
+ ?>
